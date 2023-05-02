e702a714a345ce112501ec0f26b4a9a24ed2304d
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-05-02 20:32:52 +0200

    cookie parser

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,13 @@ const mongoose = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 require('dotenv').config();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
    credentials: true,
    origin: 'http://127.0.0.1:5173'
@@ -25,12 +27,12 @@ app.get('/test', (req,res) => {
 app.post('/register', async (req,res) => {
    const { name, email, password } = req.body;
    try {
-      const newRegisteredUser = await User.create({
+      const newUser = await User.create({
          name,
          email,
          password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
       });
-      res.json(newRegisteredUser);
+      res.json(newUser);
    } catch (e) {
       res.status(422).json(e);
    }
@@ -42,15 +44,32 @@ app.post('/login', async (req,res) => {
    if (foundUser) {
       const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
       if (passwordCorrect) {
-         jwt.sign({ email: foundUser.email, id: foundUser._id }, process.env.JWT_SECRET, {}, (err, token) => {
+         jwt.sign({ 
+            id: foundUser._id, 
+            email: foundUser.email, 
+            name: foundUser.name 
+         }, process.env.JWT_SECRET, {}, (err, token) => {
             if (err) throw err;
-            res.cookie('myCookie', token).json('Password ok');  // options: { secure: false, sameSite: 'none'}
+            res.cookie('token', token).json(foundUser);
          });
       } else {
-         res.status(422).json('Password not ok');
+         res.status(422).json('Password not correct');
       }
    } else {
-      res.json('User not found');
+      res.status(400).json('User not found').end();
+   }
+});
+
+app.get('/profile', (req,res) => {
+   const { token } = req.cookies;
+   if (token) {
+      // Decrypt token with the secret key
+      jwt.verify(token, process.env.JWT_SECRET, {}, (err, userData) => {
+         if (err) throw err;
+         res.json(userData);
+      });
+   } else {
+      res.json(null);
    }
 });
 
