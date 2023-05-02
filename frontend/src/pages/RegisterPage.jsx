b4caf080e5aca69cb4b9d1f5d7fd950c3afe002c
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-05-02 10:12:45 +0200

    register

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,15 +1,34 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 const RegisterPage = () => {
+   const [name, setName] = useState('');
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+   const registerUser = async (e) => {
+      e.preventDefault();
+      try {
+         await axios.post('/register', {
+            name,
+            email,
+            password
+         });
+         alert('Registration successful. Now you can log in');
+      } catch (exception) {
+         alert('Registration failed. Please try again later');
+      }
+   }
+
   return (
    <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
          <h1 className="text-4xl text-center mb-4">Register</h1>
-         <form className="max-w-md mx-auto">
-            <input type="text" placeholder="John Doe" />
-            <input type="email" placeholder="your@email.com" />
-            <input type="password" placeholder="password" />
-            <button className="primary">Login</button>
+         <form className="max-w-md mx-auto" onSubmit={registerUser}>
+            <input type="text" placeholder="John Doe" value={name} onChange={e => setName(e.target.value)} />
+            <input type="email" placeholder="your@email.com" value={email} onChange={e => setEmail(e.target.value)} />
+            <input type="password" placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
+            <button className="primary">Register</button>
             <div className="text-center py-2 text-gray-500">
                Already a member? {" "} 
                <Link to={'/login'} className="underline text-black">
