5d90d67ff3498bb6e52f5d533240ec4320d483cb
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-05-02 10:12:35 +0200

    server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,38 @@
-// Api routes
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+
+require('dotenv').config();
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+   credentials: true,
+   origin: 'http://127.0.0.1:5173'
+}));
+
+mongoose.connect(process.env.MONGO_URL);
+
+// Endpoints
+
+app.get('/test', (req,res) => {
+   res.json(('test ok'))
+});
+
+app.post('/register', async (req,res) => {
+   const { name, email, password } = req.body;
+   try {
+      const newUser = await User.create({
+         name,
+         email,
+         password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+      });
+      res.json(newUser);
+   } catch (e) {
+      res.status(422).json(e);
+   }
+});
+
+app.listen(4000);
