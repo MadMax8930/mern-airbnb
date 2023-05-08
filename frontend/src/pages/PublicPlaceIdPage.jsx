455fed99225ed02d2fd90f01ffbf37690385873a
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-05-08 11:49:20 +0200

    booking widget

--- frontend/src/pages/PublicPlaceIdPage.jsx
+++ frontend/src/pages/PublicPlaceIdPage.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import BookingWidget from "../components/BookingWidget";
 import axios from "axios";
 
 const PublicPlaceIdPage = () => {
@@ -18,7 +19,7 @@ const PublicPlaceIdPage = () => {
       <div className="absolute inset-0 bg-black text-white min-h-screen">
          <div className="grid gap-4 p-8 bg-black">
             <div>
-               <h2 className="text-3xl">Photos of {place.title}</h2>
+               <h2 className="text-3xl mr-48">Photos of {place.title}</h2>
                <button onClick={() => setShowAllPhotos(false)} className="fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow bg-white text-black">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                      <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
@@ -35,7 +36,7 @@ const PublicPlaceIdPage = () => {
    )
 
   return (
-   <div className="bg-gray-100 mt-4 py-8 -mx-8 px-8">
+   <div className="bg-gray-100 mt-4 pt-8 -mx-8 px-8">
       <h1 className="text-3xl">{place.title}</h1>
       <a target="_blank" rel="noreferrer" href={'https://maps.google.com/?q=' + place.address} className="flex font-semibold underline cursor-pointer my-3 gap-1">
          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-5 h-6">
@@ -47,14 +48,17 @@ const PublicPlaceIdPage = () => {
          <div className="grid grid-cols-[2fr_1fr] gap-2 rounded-3xl overflow-hidden">
             <div>
                {place.photos?.[0] && (
-                  <img src={`http://localhost:4000/uploads/${place.photos[0]}`} className="aspect-square object-cover" alt="Place picture 1" />)}
+                  <img src={`http://localhost:4000/uploads/${place.photos[0]}`} onClick={() => setShowAllPhotos(true)} 
+                       className="aspect-square object-cover cursor-pointer" alt="Picture 1" />)}
             </div>
             <div>
                {place.photos?.[1] && (
-                  <img src={`http://localhost:4000/uploads/${place.photos[1]}`} className="aspect-square object-cover" alt="Place picture 2" />)}
+                  <img src={`http://localhost:4000/uploads/${place.photos[1]}`} onClick={() => setShowAllPhotos(true)} 
+                       className="aspect-square object-cover cursor-pointer" alt="Picture 2" />)}
                {place.photos?.[2] && (
                <div className="overflow-hidden">
-                  <img src={`http://localhost:4000/uploads/${place.photos[2]}`} className="aspect-square object-cover relative top-2" alt="Place picture 3" />
+                  <img src={`http://localhost:4000/uploads/${place.photos[2]}`} onClick={() => setShowAllPhotos(true)} 
+                       className="aspect-square object-cover cursor-pointer relative top-2" alt="Picture 3" />
                </div>)}
             </div>
          </div>
@@ -63,6 +67,24 @@ const PublicPlaceIdPage = () => {
                <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clipRule="evenodd" />
             </svg> Show more photos
          </button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-[2fr_1fr] mt-7 mb-7 gap-7">
+         <div>
+            <div className="my-4">
+               <h2 className="font-semibold text-2xl">Description</h2>
+               {place.description}
+            </div>
+            Check-in: {place.checkIn} <br />
+            Check-out: {place.checkOut} <br />
+            Max number of guests: {place.maxGuests}
+         </div>
+         <div>
+            <BookingWidget place={place} />
+         </div>
+      </div>
+      <div className="bg-white -mx-8 px-8 py-8 border-t">
+         <div><h2 className="font-semibold text-2xl">Extra info</h2></div>
+         <div className="text-gray-700 text-sm leading-5 mb-4 mt-2">{place.extraInfo}</div>
       </div>
    </div>
   )
