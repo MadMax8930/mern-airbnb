4020159e424687bc36b6083840f39bb786b136e6
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-05-09 19:47:15 +0200

    svg refactoring

---
+++ frontend/src/svg/index.js
@@ -0,0 +1,53 @@
+import ListSvg from './ListSvg';
+import LogoSvg from './LogoSvg';
+import MoneySvg from './MoneySvg';
+import MoonSvg from './MoonSvg';
+import ParkingSvg from './ParkingSvg';
+import PetsSvg from './PetsSvg';
+import PictureSvg from './PictureSvg';
+import PinSvg from './PinSvg';
+import PlaceSvg from './PlaceSvg';
+import PlusSvg from './PlusSvg';
+import PointerSvg from './PointerSvg';
+import PrivateEntranceSvg from './PrivateEntranceSvg';
+import ProfileSvg from './ProfileSvg';
+import RadioSvg from './RadioSvg';
+import RightArrowSvg from './RightArrowSvg';
+import SearchSvg from './SearchSvg';
+import StarFilledSvg from './StarFilledSvg';
+import StarUnfilledSvg from './StarUnfilledSvg';
+import TimeDateSvg from './TimeDateSvg';
+import TrashBinSvg from './TrashBinSvg';
+import TvSvg from './TvSvg';
+import UploadSvg from './UploadSvg';
+import UserSvg from './UserSvg';
+import WifiSvg from './WifiSvg';
+import XcrossSvg from './XcrossSvg';
+
+export {
+  ListSvg,
+  LogoSvg,
+  MoneySvg,
+  MoonSvg,
+  ParkingSvg,
+  PetsSvg,
+  PictureSvg,
+  PinSvg,
+  PlaceSvg,
+  PlusSvg,
+  PointerSvg,
+  PrivateEntranceSvg,
+  ProfileSvg,
+  RadioSvg,
+  RightArrowSvg,
+  SearchSvg,
+  StarFilledSvg,
+  StarUnfilledSvg,
+  TimeDateSvg,
+  TrashBinSvg,
+  TvSvg,
+  UploadSvg,
+  UserSvg,
+  WifiSvg,
+  XcrossSvg,
+}
