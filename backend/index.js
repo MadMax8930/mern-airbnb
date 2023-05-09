8f562baa543333bdc27faf96012ceb8f00dec1a4
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-05-09 17:15:23 +0200

    api endpoints full

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,39 @@
 const express = require('express');
-const cors = require('cors'); // front/back communication
-const mongoose = require('mongoose'); // db connection
-const User = require('./models/User');
-const Place = require('./models/Place');
-const bcrypt = require('bcryptjs');  // encrypt password
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const imageDownloader = require('image-downloader');
-const imageUploader = require('multer'); // upload pictures
-const fs = require('fs') // rename files on the server
+const imageUploader = require('multer');
+const fs = require('fs');
+const User = require('./models/User');
+const Place = require('./models/Place');
+const Booking = require('./models/Booking');
 
 require('dotenv').config();
 const app = express();
 
-app.use(express.json()); // app should use json parser (we need to parse the json from req)
+app.use(express.json());
 app.use(cookieParser());
-app.use('/uploads', express.static(__dirname + '/uploads'));  // Mapping server to dirname path (links -> photos)
+app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
    credentials: true,
    origin: 'http://127.0.0.1:5173'
 }));
 
 mongoose.connect(process.env.MONGO_URL);
+
+// Helper
+
+function getUserData(req) {
+   return new Promise((resolve, reject) => {
+      jwt.verify(req.cookies.token, process.env.JWT_SECRET, {}, async (err, userData) => {
+         if (err) throw err;
+         resolve(userData); 
+      }); 
+   });
+}
 
 // Endpoints
 
@@ -151,6 +163,24 @@ app.get('/public-places/:id', async (req,res) => {
    const { id } = req.params;
    const thatOnePlace = await Place.findById(id);
    res.json(thatOnePlace);
+});
+
+app.post('/bookings', async (req,res) => {
+   const userData = await getUserData(req);
+   const { place, checkIn, checkOut, numOfGuests, name, phone, price } = req.body;
+   Booking.create({
+      place, checkIn, checkOut, numOfGuests, name, phone, price, user: userData.id
+   }).then((newBooking) => {
+      res.json(newBooking);
+   }).catch((err) => {
+      throw err;
+   });
+});
+
+app.get('/bookings', async (req,res) => {
+   const userData = await getUserData(req);
+   const userBookings = await Booking.find({ user: userData.id }).populate('place');
+   res.json(userBookings);
 });
 
 app.listen(4000);
